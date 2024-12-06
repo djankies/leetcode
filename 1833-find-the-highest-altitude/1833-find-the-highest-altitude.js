1bf638dd1814a47fea7798023803d6e70ade884a
Daniel Jankowski <92045617+djankies@users.noreply.github.com>
2024-12-06 17:35:37 -0500

    Time: 0 ms (100%), Space: 48.7 MB (76.44%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function (gain) {
+    let current = 0;
+    let max = 0;
+
+    for (let i = 0; i < gain.length; i++) {
+        current = current + gain[i];
+        max = Math.max(max, current);
+    }
+
+    return max;
+};
