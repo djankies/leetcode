49c2306567f38810dbfa9a8ff7631b47743b75f5
Daniel Jankowski <djank0113@gmail.com>
2024-12-28 16:50:12 -0500

    Time: 0 ms (100%), Space: 49.5 MB (24.14%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -13,11 +13,6 @@
  */
 var isSameTree = function(p, q) {
     if (!p && !q) return true;
-    if (!p !== !q) return false;
-    if (p.val !== q.val 
-    || !p.left !== !q.left 
-    || !q.right !== !q.right) return false;
-    const left = isSameTree(p.left, q.left);
-    const right = isSameTree(p.right, q.right);
-    return left && right;
+    if (!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
