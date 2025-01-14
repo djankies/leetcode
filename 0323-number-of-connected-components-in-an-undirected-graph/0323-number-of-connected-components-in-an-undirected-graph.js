c16e558033f393ee4f62d6c93580b2f85853c6a5
Daniel Jankowski <djank0113@gmail.com>
2025-01-14 11:56:36 -0500

    Time: 7 ms (80%), Space: 54 MB (64%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
@@ -4,32 +4,32 @@
  * @return {number}
  */
 var countComponents = function(n, edges) {
-    const graph = new Map();
+    const graph = {};
     
     for (let i = 0; i < n; i++) {
-        graph.set(i, []);
+        graph[i] = [];
     }
     
     for(const [a, b] of edges) {
-        graph.get(a).push(b);
-        graph.get(b).push(a);
+        graph[a].push(b);
+        graph[b].push(a);
     }
     
     let count = 0;
     
-    const seen = new Set();
+    const seen = Array(n).fill(false);
     
     const dfs = (node) => {
-        seen.add(node);
-        for (const neighbor of graph.get(node)) {
-            if (!seen.has(neighbor)) {
+        seen[node] = true;
+        for (const neighbor of graph[node]) {
+            if (!seen[neighbor]) {
                 dfs(neighbor);
             }
         }
     }
     
     for (let i = 0; i < n; i++) {
-        if (!seen.has(i)) {
+        if (!seen[i]) {
             dfs(i);
             count++;
         }
